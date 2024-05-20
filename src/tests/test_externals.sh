580c26bc6acc627992734002dd74c5d2c06d6213
Solar Designer <solar@openwall.com>
2024-05-20 17:50:49 +0200

    make check, CI: Test only 10k candidates per external mode, add KDEPaste

--- src/tests/test_externals.sh
+++ src/tests/test_externals.sh
@@ -7,10 +7,12 @@ set -x
 for mode in `$JOHN --list=ext-modes`; do
 # KDEPaste's output depends on the current time
 	if [ $mode != kdepaste ]; then
-		$JOHN --external=$mode --stdout --max-candidates=100000
+		$JOHN --external=$mode --stdout --max-candidates=10000
 	fi
 done
 
+$JOHN --external=KDEPaste --stdout --max-candidates=10000 | tr -c '\n' .
+
 for mode in `$JOHN --list=ext-hybrids` `./john --list=ext-filters`; do
-	$JOHN -w --external=$mode --stdout --max-candidates=100000
+	$JOHN -w --external=$mode --stdout --max-candidates=10000
 done
