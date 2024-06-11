95a74b7ab84612c69ca75ffc0d64aa588286725c
Claudio André <dev@claudioandre.slmail.me>
2024-06-11 10:14:34 -0300

    release: deliver a new 1.9.1-ce tarball file
    Define RELEASE_BUILD and make a short announcement about it.
    
    Signed-off-by: Claudio André <dev@claudioandre.slmail.me>

---
+++ RELEASE-NOTES.md
@@ -0,0 +1,18 @@
+# Release Notes
+
+This is a bug fix, renovate, and update release intended to provide a
+modernized version to end users and packagers. "Early and Often".
+
+There have been over 1300 tree commits since we released 1.9.0-jumbo-1.
+Only the source code tarball is published right now.
+
+Major changes from 1.9.0-jumbo-1 (May 2019) to 1.9.1-ce (June 2024):
+
+- The development team now prioritizes code quality improvements and
+  issues resolution. So, they brought to you a lot of relevant major changes:
+
+  - New formats, external modes, and 2john tools;
+  - Optimizations, improvements, and fixes everywhere.
+
+A more complete list of changes that have occurred since 1.9.0-jumbo-1 can
+be seen at [NEWS](doc/NEWS) file.
--- src/params.h
+++ src/params.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of John the Ripper password cracker,
- * Copyright (c) 1996-2019 by Solar Designer
+ * Copyright (c) 1996-2024 by Solar Designer
  *
  * ...with changes in the jumbo patch, by various authors
  *
@@ -26,22 +26,22 @@
 /*
  * John's version number.
  */
-#define JOHN_VERSION			"1.9.0"
+#define JOHN_VERSION			"1.9.1"
 
 /*
  * Define this for release tarballs. It affects the version reporting (will
  * be the string above and below and never a Git hash).
  */
-//#define JTR_RELEASE_BUILD 1
+#define JTR_RELEASE_BUILD 1
 
 /*
  * Jumbo's version number. Note that we must uncomment JTR_RELEASE_BUILD
  * above, in any release tar-balls (and only then, never ever in Git).
  */
-#define JUMBO_POSTFIX			"-jumbo-1"
+#define JUMBO_POSTFIX			"-ce"
 
 #if JTR_RELEASE_BUILD
-#define JUMBO_VERSION			JOHN_VERSION JUMBO_POSTFIX
+#define JUMBO_VERSION			JOHN_VERSION JUMBO_POSTFIX JTR_RELEASE_VERSION
 #else
 #define JUMBO_VERSION			JOHN_VERSION JUMBO_POSTFIX "+bleeding"
 #endif
