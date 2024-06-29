00f7934ef6a2ee34d2a2eced4988a925d32e7923
Solar Designer <solar@openwall.com>
2024-06-29 20:56:50 +0200

    SM3 format: Add second test vector
    from https://datatracker.ietf.org/doc/html/draft-sca-cfrg-sm3#appendix-C.3

--- src/sm3_fmt_plug.c
+++ src/sm3_fmt_plug.c
@@ -50,6 +50,8 @@ john_register_one(&fmt_sm3);
 
 static struct fmt_tests sm3_tests[] = {
 	{ "66c7f0f462eeedd9d1f2d46bdc10e4e24167c4875cf2f7a2297da02b8f4ba8e0", "abc" },
+	{ "debe9ff92275b8a138604889c18e5a4d6fdb70e5387e5765293dcba39c0c5732",
+	  "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd" },
 	{ "$sm3$66c7f0f462eeedd9d1f2d46bdc10e4e24167c4875cf2f7a2297da02b8f4ba8e0", "abc" },
 	{ NULL }
 };
